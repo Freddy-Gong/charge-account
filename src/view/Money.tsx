98653ad6d32c1a723cdffd0cb0d23df13015e6c0
Freedy <21914103@zju.edu.cn>
2020-08-12 14:14:54 +0800

    category切换功能搞定

--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -17,12 +17,7 @@ function Money() {
         <MyLayout>
             <TagSection />
             <NotesSection />
-            <CategorySection>
-                <ul>
-                    <li className="selected">支出</li>
-                    <li>收入</li>
-                </ul>
-            </CategorySection>
+            <CategorySection />
             <NumberPadSection>
                 <div className="calculator">
                     <div className="result">100</div>
--- src/view/Money/CategorySection.tsx
+++ src/view/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
+import React, { useState } from 'react'
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
     > ul {
         display:flex;
         background:rgb(196,195,197);
@@ -23,4 +24,22 @@ const CategorySection = styled.section`
         }
     }
 `
+
+const CategorySection: React.FC = () => {
+    const [categoryList] = useState<('-' | '+')[]>(['-', '+'])
+    const [category, setCategory] = useState('-')
+    const categoryMap = { '-': '支出', '+': '收入' }
+    return (
+        <Wrapper>
+            <ul>
+                {categoryList.map(c =>
+                    <li className={category === c ? 'selected' : ''}
+                        onClick={() => setCategory(c)} key={c}
+                    >{categoryMap[c]}</li>
+                )}
+            </ul>
+        </Wrapper>
+    )
+}
+
 export default CategorySection
