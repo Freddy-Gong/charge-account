4d34de2b156e473688b7a0c0f9c5bf239d4fe7e5
Freedy <21914103@zju.edu.cn>
2020-08-14 16:38:42 +0800

    Tag页面样式更改完成

--- src/view/Tag.tsx
+++ src/view/Tag.tsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import { useTags } from 'useTags'
 import { useParams } from 'react-router-dom'
+import Layout from 'components/layout'
+import styled from 'styled-components'
+import Icon from 'components/icon'
 type Params = {
     id: string
 }
+
+const TopBar = styled.div`
+    display:flex;
+    justify-content:space-between;
+    align-item:center;
+    padding:16px 10px;
+    font-size: 24px;
+    font-family: Helvetica;
+    font-weight: normal;
+    color: #999;
+    background: linear-gradient(135deg,rgba(230,230,230,1) 0%,rgba(246,246,246,1) 100%);
+    box-shadow: -4px -4px 10px -8px rgba(255,255,255,1), 4px 4px 10px -8px rgba(0, 0, 0, .3);
+    border-radius:50px
+`
+const Button = styled.button`
+    margin: 0 100px;
+    font-size: 24px;
+    font-family: Helvetica;
+    font-weight: normal;
+    color: #999;
+    border-radius:20px;
+    padding:10px 16px;
+    border:none;
+    background: linear-gradient(135deg,rgba(230,230,230,1) 0%,rgba(246,246,246,1) 100%);
+    box-shadow: -4px -4px 10px -8px rgba(255,255,255,1), 4px 4px 10px -8px rgba(0, 0, 0, .3);
+`
+const Name = styled.div`
+    margin:8px 0;
+    padding:16px;
+    font-size: 24px;
+    font-family: Helvetica;
+    font-weight: normal;
+    color: #999;
+    border-radius:20px;
+    background: linear-gradient(135deg,rgba(230,230,230,1) 0%,rgba(246,246,246,1) 100%);
+    box-shadow: -4px -4px 10px -8px rgba(255,255,255,1), 4px 4px 10px -8px rgba(0, 0, 0, .3);
+    > label {
+        display:flex;
+        align-item:center;
+        > span {
+            margin-right:8px;
+            white-space:nowrap;
+        }
+        > input {
+            display:block;
+            width:100%;
+            border:none;
+            background: linear-gradient(135deg,rgba(230,230,230,1) 0%,rgba(246,246,246,1) 100%);
+            font-size: 24px;
+            font-family: Helvetica;
+            font-weight: normal;
+            color: #999;
+        }
+    }
+`
+const Space = styled.div`
+    height:32px;
+`
 
 const Tag: React.FC = () => {
     const { findTag } = useTags()
@@ -11,7 +72,21 @@ const Tag: React.FC = () => {
 
     const tag = findTag(parseInt(id))
     return (
-        <div>{tag.name}</div>
+        <Layout>
+            <TopBar>
+                <Icon name="left" />
+                <span>编辑标签</span>
+                <Icon name='' />
+            </TopBar>
+            <Name>
+                <label>
+                    <span>标签名</span>
+                    <input type="text" placeholder={tag.name} />
+                </label>
+            </Name>
+            <Space />
+            <Button>删除标签</Button>
+        </Layout>
     )
 }
 export default Tag
