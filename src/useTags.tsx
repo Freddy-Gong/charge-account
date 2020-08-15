de14708b65b8309597b48810a2c8e0a1268b6a85
Freedy <21914103@zju.edu.cn>
2020-08-15 10:10:27 +0800

    删除tag功能完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -26,12 +26,22 @@ const useTags = () => { // 封装一个自定义Hook
         tagsClone.splice(index, 1, { id: id, name: obj.name })
         setTags(tagsClone)
     }
+    const deleteTag = (id: number) => {
+        //获取你要改的 tag 的下标
+        const index = findTagIndex(id)
+        //深拷贝 tags 得到tagsClone
+        const tagsClone = JSON.parse(JSON.stringify(tags))
+        //把 tagsClone 的第index删掉，替换成{id:id,name:obj.name}
+        tagsClone.splice(index, 1)
+        setTags(tagsClone)
+    }
     return {
         tags,
         setTags,
         findTag,
         updateTag,
-        findTagIndex
+        findTagIndex,
+        deleteTag,
     }
 }
 
--- src/view/Tag.tsx
+++ src/view/Tag.tsx
@@ -57,7 +57,7 @@ const Center = styled.div`
 `
 
 const Tag: React.FC = () => {
-    const { findTag, updateTag } = useTags()
+    const { findTag, updateTag, deleteTag } = useTags()
     let { id: idString } = useParams<Params>()
 
     const tag = findTag(parseInt(idString))
@@ -68,13 +68,16 @@ const Tag: React.FC = () => {
                 <span>编辑标签</span>
                 <Icon name='' />
             </TopBar>
-            <Name>
-                <Input label="标签名" value={tag.name} onChange={(e) => updateTag(tag.id, { name: e.target.value })} />
-            </Name>
-            <Space />
-            <Center>
-                <Button>删除标签</Button>
-            </Center>
+            {tag ? <div>
+                <Name>
+                    <Input label="标签名" value={tag.name} onChange={(e) => updateTag(tag.id, { name: e.target.value })} />
+                </Name>
+                <Space />
+                <Center>
+                    <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
+                </Center>
+            </div> : <Center> tag 已删除</Center>}
+
         </Layout>
     )
 }
